c9b901d28b7fb93324911f2dd0b321dd5ee765aa
tawounfouet <thomas.awounfouet@gmail.com>
2024-01-27 23:57:38 +0000

    udpate

--- Project/settings.py
+++ Project/settings.py
@@ -136,12 +136,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join((BASE_DIR), "media")
 
 # Default primary key field type
